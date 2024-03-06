789568d75c36c5f60cb0b46fde43abf21f6eb028
Laura <268312@student.pwr.edu.pl>
2024-03-06 14:50:54 +0100

    creating methods: add, substract

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    public int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    public int subtract(int a, int b){
+        int difference = a - b;
+        return difference;
+    }
 }
--- src/CalculatorLauncher.java
+++ src/CalculatorLauncher.java
@@ -1,3 +1,5 @@
 public class CalculatorLauncher {
-    System.out.println("Coming soon!");
+    public static void main(String[] args) {
+        System.out.println("Coming soon!");
+    }
 }
